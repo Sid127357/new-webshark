aaa0fd028e4bb8ff4da8588f8d574fcec123aa9e
Sid127357 <sidharthkaushal369@gmail.com>
2024-08-28 20:42:36 +0530

    added the dummy page

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var portfolioBox = document.querySelector(".img-box")
+var cursor = document.querySelector(".cursor-view-more");
+var mainBox = document.querySelector("main")
+
+mainBox.addEventListener("mousemove", function (ele) {
+    gsap.to(cursor,{
+        x : ele.x,
+        y : ele.y
+    })
+})
+portfolioBox.addEventListener("mouseenter", function () {
+    cursor.innerHTML = "View More"
+    gsap.to(cursor, {
+        
+        scale: 2
+    });
+    // console.log(event);
+
+});
+portfolioBox.addEventListener("mouseleave", function () {
+    cursor.innerHTML = ""
+    gsap.to(cursor, {
+        scale: 1
+    });
+    // console.log(event);
+
+});
